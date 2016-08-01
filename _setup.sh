043eefdb15e4ed7d726b0a101f72b13332fba7d9
Taichi <taichi@curipha.jp>
2016-08-02 00:58:53 +0900

    Drop screenrc from setup script

--- _setup.sh
+++ _setup.sh
@@ -6,7 +6,7 @@
 set -o nounset
 set -o errexit
 
-DOTFILES=( gemrc gitconfig gvimrc inputrc irbrc pryrc screenrc tmux.conf vimrc zshrc )
+DOTFILES=( gemrc gitconfig gvimrc inputrc irbrc pryrc tmux.conf vimrc zshrc )
 SSH_CONFIG=ssh_config
 
 abort() {
